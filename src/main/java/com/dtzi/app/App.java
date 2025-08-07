ddf444954ad163ddad950f495035b290fd2c4a23
Dtzibell <zaleniakas.tauras@gmail.com>
2025-08-07 14:41:44 +0200

    created expenses tracker

--- src/main/java/com/dtzi/app/App.java
+++ src/main/java/com/dtzi/app/App.java
@@ -1,30 +1,49 @@
 package com.dtzi.app;
 import java.util.*;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
 public class App {
   public static void main(String[] args) {
     ExpensesCalculator tracker = new ExpensesCalculator();
-    Integer action = tracker.getAction();
-    switch (action) {
-      case 1: tracker.getIncomeType();
-              break;
-      case 2: tracker.getExpenseType();
-              break;
-      case 4: for (int a = 0; a < tracker.transactionType.size(); a++) {
-              System.out.println("Transaction type: " + tracker.transactionType.get(a));
-              System.out.println("Expense/income type: " + tracker.expenseType.get(a));
-              System.out.println("Amount: " + tracker.amounts.get(a));
-              System.out.println("Date: " + tracker.dates.get(a));
-              }
-              break;
-      default: System.out.print("not implemented");
-               break;
+    while (true) {
+      Integer action = tracker.getAction();
+      switch (action) {
+        case 1: tracker.getIncomeType();
+                tracker.getAmount();
+                tracker.getDate();
+                break;
+        case 2: tracker.getExpenseType();
+                tracker.getAmount();
+                tracker.getDate();
+                break;
+        case 3: tracker.getSummary();
+                break;
+                /* This is potentially turnable into function, too lazy.
+                 * Could cache everything into a dict real time while inputting
+                 * expenses.
+                 */
+        case 4: System.out.println("===== List of transactions =====");
+                for (int a = 0; a < tracker.transactionType.size(); a++) {
+                  ExpensesCalculator.TransactionType transactionType = ExpensesCalculator.TransactionType.byIndex(tracker.transactionType.get(a));
+                  System.out.println("Transaction type: " + transactionType);
+                  if (transactionType == ExpensesCalculator.TransactionType.INCOME) {
+                    ExpensesCalculator.IncomeType moneyPurpose = ExpensesCalculator.IncomeType.byIndex(tracker.expenseType.get(a));
+                    System.out.println("Income type: " + moneyPurpose);
+                  }
+                  else {
+                    ExpensesCalculator.ExpenseType moneyPurpose = ExpensesCalculator.ExpenseType.byIndex(tracker.expenseType.get(a));
+                    System.out.println("Expense type: " + moneyPurpose);
+                  }
+                  System.out.println("Amount: " + tracker.amounts.get(a));
+                  System.out.println("Date: " + tracker.dates.get(a) + "\n");
+                }
+                break;
+        default: System.out.print("not implemented");
+                 break;
       }
-    // tracker.getExpenseType();
-    // tracker.getAmount();
-    // tracker.getDate();
+    }
   }
 }
 
@@ -33,6 +52,67 @@ class ExpensesCalculator {
   List<Double> amounts;
   List<LocalDate> dates; // use parse of String with some try-catch
   Scanner inputScanner; 
+  Integer action;
+  enum TransactionType {
+    INCOME(1), EXPENSE(2);
+    
+    public final int index;
+    private TransactionType (int index) {
+      this.index = index;
+    }
+
+    private static final Map<Integer, TransactionType> TRANSACTIONS = new HashMap<>();
+    
+    static { 
+      for (TransactionType t: values()) {
+        TRANSACTIONS.put(t.index, t);
+      }
+    }
+
+    static TransactionType byIndex (int index) {
+      return TRANSACTIONS.get(index);
+    }
+  }
+  enum ExpenseType {
+    FOOD(1), RENT(2), TRANSPORT(3), ENTERTAINMENT(4), OTHER(5);
+
+    public final int index;
+    private ExpenseType (int index) {
+      this.index = index;
+    }
+
+    private static final Map<Integer, ExpenseType> TRANSACTIONS = new HashMap<>();
+    
+    static { 
+      for (ExpenseType t: values()) {
+        TRANSACTIONS.put(t.index, t);
+      }
+    }
+
+    static ExpenseType byIndex (int index) {
+      return TRANSACTIONS.get(index);
+    }
+  }
+  enum IncomeType {
+    SALARY(1), GIFT(2), ALIMONY(3), OTHER(4);
+    
+    public final int index;
+    private IncomeType (int index) {
+      this.index = index;
+    }
+
+    private static final Map<Integer, IncomeType> TRANSACTIONS = new HashMap<>();
+    
+    static { 
+      for (IncomeType t: values()) {
+        TRANSACTIONS.put(t.index, t);
+      }
+    }
+
+    static IncomeType byIndex (int index) {
+      return TRANSACTIONS.get(index);
+    }
+  }
 
   ExpensesCalculator(){
     inputScanner = new Scanner(System.in);
@@ -43,7 +123,6 @@ class ExpensesCalculator {
   }
 
   Integer getAction() {
-    Integer action;
     String lines = "===== Personal expenses tracker ===== \n" + 
         "1. Add income \n" +
         "2. Add expense \n" +
@@ -51,12 +130,16 @@ class ExpensesCalculator {
         "4. View all transactions \n" +
         "5. Exit";
     System.out.println(lines);
-    Set<Integer> validOptions = Set.of(1,2,3,4);
+    Set<Integer> validPurposes = Set.of(1,2);
+    Set<Integer> validFunctions = Set.of(3,4);
     while (true) {
       if (inputScanner.hasNextInt()) {
         action = inputScanner.nextInt();
-        if (validOptions.contains(action)){
-          expenseType.add(action);
+        if (validPurposes.contains(action)){
+          transactionType.add(action);
+          break;
+        }
+        else if (validFunctions.contains(action)) {
           break;
         }
         else if (action == 5) {
@@ -64,8 +147,8 @@ class ExpensesCalculator {
           System.exit(0);
         }
       }
-      System.out.print("Invalid input. Enter a number between 1 and 5: ");
       inputScanner.next();
+      System.out.print("Invalid input. Enter a number between 1 and 5: ");
     }
     return action;
   }
@@ -83,12 +166,12 @@ class ExpensesCalculator {
       if (inputScanner.hasNextInt()) {
         action = inputScanner.nextInt();
         if (validOptions.contains(action)){
-          transactionType.add(inputScanner.nextInt());
+          expenseType.add(action);
           break;
         }
       }
-      System.out.print("Invalid input. Please enter a number between 1 and 5: ");
       inputScanner.next();
+      System.out.print("Invalid input. Please enter a number between 1 and 5: ");
     }
   }
   void getIncomeType() {
@@ -98,24 +181,34 @@ class ExpensesCalculator {
       "3. Alimony \n" + 
       "4. Other";
     System.out.println(lines);
+    Set<Integer> validOptions = Set.of(1,2,3,4);
     while (true) {
       if (inputScanner.hasNextInt()) {
-        transactionType.add(inputScanner.nextInt());
-        break;
+        action = inputScanner.nextInt();
+        if (validOptions.contains(action)){
+          expenseType.add(action);
+          break;
+        }
       }
-      System.out.print("Invalid input. Please enter a number between 1 and 4: ");
       inputScanner.next();
+      System.out.print("Invalid input. Please enter a number between 1 and 4: ");
     }
   }
   void getAmount() {
     System.out.print("Enter the amount: ");
     while (true){
       if (inputScanner.hasNextDouble()) {
-        amounts.add(inputScanner.nextDouble());
-        break;
+        if (transactionType.getLast() == 1) {
+          amounts.add(inputScanner.nextDouble());
+          break;
+        }
+        else {
+          amounts.add(-inputScanner.nextDouble());
+          break;
+        }
       }
-      System.out.print("Invalid input. Please enter a dot denominated decimal: ");
       inputScanner.next();
+      System.out.print("Invalid input. Please enter a dot denominated decimal: ");
     }
   }
   void getDate() {
@@ -125,10 +218,32 @@ class ExpensesCalculator {
         String input = inputScanner.next();
         LocalDate date = LocalDate.parse(input);
         dates.add(date);
+        break;
       } 
       catch (DateTimeParseException e) {
-        inputScanner.next();
+        System.out.print("Incorrect date format. Try YYYY-MM-DD: ");
       }
     }
+  }
+  void getSummary(){
+    System.out.println("===== Summary =====");
+    Double sumOfIncoming = 0.0d;
+    Double sumOfOutgoing = 0.0d;
+    Double amount;
+    for (int i = 0; i < amounts.size(); i++) {
+      amount = amounts.get(i);
+      if (amount < 0) {
+        sumOfOutgoing += amount;
+      }
+      else {
+        sumOfIncoming += amount;
+      }
+    }
+    System.out.print("Total balance: ");
+    System.out.println(sumOfIncoming + sumOfOutgoing);
+    System.out.print("Total income: ");
+    System.out.println(sumOfIncoming);
+    System.out.print("Total expenses: ");
+    System.out.println(-sumOfOutgoing);
   }
 }
